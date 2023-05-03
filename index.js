e4888da9445783686cc7c7568032a37614f28ffb
MANSOURIFIRAS <mansouri.firas@esprit.tn>
2023-05-03 05:24:02 +0200

    change index.js code

--- index.js
+++ index.js
@@ -1,120 +1,14 @@
+// Import packages
+const express = require("express");
+const home = require("./routes/home");
 
-
-const express = require('express');
-
-// const connect = require('./Infrastructure/Database/mongodb');
-// const makeCreateUser = require('./Application/UseCases/user/createUser');
-// const UserRepository = require('./Domain/IRepositories/UserRepository');
-// const makeUserController = require('./Presentation/Controllers/userController');
-// const makeUserRoutes = require('./Presentation/Routes/userRoutes');
-// const UserModel = require('./Infrastructure/Models/UserModel');
-const cors = require('cors');
-
+// Middlewares
 const app = express();
-const passport = require('passport');
-const multer = require('multer');
-
-var cookieSession = require('cookie-session');//
-require('./src/Presentation/middlwares/passport');
-
-// initial route
-app.get("/", (req, res) => {
-  res.json({ message: "School space backend initial ..." });
-});
-var usersRouter = require('./src/Presentation/routes/users');
-var contractroutes=require('./src/Presentation/routes/contractroutes');
-var productsRouter = require('./src/Presentation/routes/products');
-var googleRouter = require('./src/Presentation/routes/googleAuth');
-var shipmentroute=require('./src/Presentation/routes/shipmentroute');
-var fbRouter = require('./src/Presentation/routes/fb');
-var forgetPasswordMail = require('./src/Presentation/routes/forgetPasswordMail');
-
-var FarmRouter = require('./src/Presentation/routes/farms');
-var PlantRouter = require('./src/Presentation/routes/plants');
-var AnimalRouter = require('./src/Presentation/routes/animals');
-var ratingRouter = require('./src/Presentation/routes/rating');
-var compostsRouter = require('./src/Presentation/routes/composts');
-var commandsRouter = require('./src/Presentation/routes/commands');
-var shipmentroute=require('./src/Presentation/routes/shipmentroute');
-
-var cookieSession = require('cookie-session');
-const { json } = require( "body-parser");
-
-app.use(
-  multer({
-    limits: { fieldSize: 100 * 1024 * 1024 },
-    dest: 'uploads/',
-  }).fields([
-    { name: 'file', maxCount: 1 },
-    { name: 'video', maxCount: 1 },
-  ])
-);
-app.use(cors({
-  origin: 'http://localhost:4000',
-  methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE'],
-  credentials: true 
-}));
-
-
-
-require('dotenv').config({ path: `${__dirname}/.env` });
-
-
-app.use(cookieSession({
-	name: 'google-auth-session',
-	keys: ['key1', 'key2']
-}));
-
-
-app.use(json());
-app.set("view engine","ejs")
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
-
-
-
-app.set("view engine","ejs")
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
-app.use(passport.initialize());
-    app.use(passport.session()); 
-    app.use(cookieParser());
-
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-mongoose.connect(
-  'mongodb+srv://BioUpDataBase:4CB4OrcVWrlP1LvW@bioup.gkbagbx.mongodb.net/?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true
-  },
-  console.log('connected to database !!!!'),
-);
-
-
 app.use(express.json());
 
+// Routes
+app.use("/home", home);
 
-
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
-app.use('/google', googleRouter);
-app.use('/forget', forgetPasswordMail)
-app.use('/fb', fbRouter);
-app.use('/forget', forgetPasswordMail);
-app.use('/contract',contractroutes);
-app.use('/farms',FarmRouter);
-app.use('/plants',PlantRouter);
-app.use('/animals',AnimalRouter);
-app.use('/rating',ratingRouter)
-app.use('/composts',compostsRouter)
-app.use('/commands',commandsRouter)
-app.use('/shipment',shipmentroute);
-
-
-
-
-
-// Start the server
-
-app.listen(3000||process.env.PORT, () => {
-  console.log('Server is listening on port 3000');
-});
+// connection
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
