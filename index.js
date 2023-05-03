f46cf44ebdd5dc2925f39c50ca7b61aef39bc0f1
MANSOURIFIRAS <mansouri.firas@esprit.tn>
2023-05-03 05:39:36 +0200

    change index.js code

--- index.js
+++ index.js
@@ -12,20 +12,7 @@ var cookieSession = require('cookie-session');//
 require('./src/Presentation/middlwares/passport');
 
 
-var usersRouter = require('./src/Presentation/routes/users');
-var contractroutes=require('./src/Presentation/routes/contractroutes');
-var productsRouter = require('./src/Presentation/routes/products');
-var googleRouter = require('./src/Presentation/routes/googleAuth');
-var shipmentroute=require('./src/Presentation/routes/shipmentroute');
-var fbRouter = require('./src/Presentation/routes/fb');
-var forgetPasswordMail = require('./src/Presentation/routes/forgetPasswordMail');
 
-var FarmRouter = require('./src/Presentation/routes/farms');
-var PlantRouter = require('./src/Presentation/routes/plants');
-var AnimalRouter = require('./src/Presentation/routes/animals');
-var ratingRouter = require('./src/Presentation/routes/rating');
-var compostsRouter = require('./src/Presentation/routes/composts');
-var commandsRouter = require('./src/Presentation/routes/commands');
 
 var cookieSession = require('cookie-session');
 const { json } = require( "body-parser");
