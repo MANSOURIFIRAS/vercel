61a93914f3e59562a1cf1a99f6c3a10a10670b39
MANSOURIFIRAS <mansouri.firas@esprit.tn>
2023-05-03 05:43:23 +0200

    change index.js code

--- index.js
+++ index.js
@@ -2,59 +2,7 @@
 const express = require("express");
 const home = require("./routes/home");
 
-const cors = require('cors');
-
-const app = express();
-const passport = require('passport');
-const multer = require('multer');
-
-var cookieSession = require('cookie-session');//
-require('./src/Presentation/middlwares/passport');
-
-
-
-
-var cookieSession = require('cookie-session');
-const { json } = require( "body-parser");
-
-app.use(
-  multer({
-    limits: { fieldSize: 100 * 1024 * 1024 },
-    dest: 'uploads/',
-  }).fields([
-    { name: 'file', maxCount: 1 },
-    { name: 'video', maxCount: 1 },
-  ])
-);
-app.use(cors({
-  origin: 'http://localhost:4000',
-  methods: ['GET', 'POST', 'PUT', 'PATCH' , 'DELETE'],
-  credentials: true 
-}));
-
-
-
-require('dotenv').config({ path: `${__dirname}/.env` });
-
-
-app.use(cookieSession({
-	name: 'google-auth-session',
-	keys: ['key1', 'key2']
-}));
-
-
-app.use(json());
-app.set("view engine","ejs")
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
-
-
-
-app.set("view engine","ejs")
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
-app.use(passport.initialize());
-    app.use(passport.session()); 
-    app.use(cookieParser());
+app= express();
 
 
 app.use(express.json());
