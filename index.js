e3357a54e5727ff4e2dcdf4d6b5b2997bffc7370
MANSOURIFIRAS <mansouri.firas@esprit.tn>
2023-05-03 05:54:34 +0200

    change index.js code

--- index.js
+++ index.js
@@ -2,20 +2,7 @@
 const express = require("express");
 const home = require("./routes/home");
 
-var usersRouter = require('./src/Presentation/routes/users');
-var contractroutes=require('./src/Presentation/routes/contractroutes');
-var productsRouter = require('./src/Presentation/routes/products');
-var googleRouter = require('./src/Presentation/routes/googleAuth');
-var shipmentroute=require('./src/Presentation/routes/shipmentroute');
-var fbRouter = require('./src/Presentation/routes/fb');
-var forgetPasswordMail = require('./src/Presentation/routes/forgetPasswordMail');
 
-var FarmRouter = require('./src/Presentation/routes/farms');
-var PlantRouter = require('./src/Presentation/routes/plants');
-var AnimalRouter = require('./src/Presentation/routes/animals');
-var ratingRouter = require('./src/Presentation/routes/rating');
-var compostsRouter = require('./src/Presentation/routes/composts');
-var commandsRouter = require('./src/Presentation/routes/commands');
 
 app= express();
 
